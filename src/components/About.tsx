c205d41161444328a48abaac62aed17ce284b433
Alex Cesar <thealexcesar@gmail.com>
2023-11-24 19:44:16 -0300

    style layout image about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import Image from 'next/image';
 
 let text: string = "I am a full-stack web developer with 2 years of experience in Ruby on Rails, JavaScript, PostgreSQL"
-    text += "and SASS. I am passionate about technology and believe it can be used to improve people's lives."
-    text += "I am always looking for ways to use my skills to create useful and engaging projects."
+text += "and SASS. I am passionate about technology and believe it can be used to improve people's lives."
+text += "I am always looking for ways to use my skills to create useful and engaging projects."
+
 function About() {
+    const size = 600
     return (
         <section id='about' className='snap-center'>
-            <div className='section-wrapper'>
-                <h2 className='section-title'>About</h2>
-                <div className='mt-8 px-4'>
-                    <Image src="/about.png" alt="About Me" className="layout-image my-4" width={600} height={600} />
+            <article
+                className='flex flex-col lg:flex-row items-center justify-center text-center px-4 md:px-10 lg:px-20 pt-16'>
+                <div className='section-wrapper'>
+                    <h2 className='section-title'>About</h2>
+                    <Image src="/about.png" alt="About Me" className="layout-image mt-8 px-0" width={size} height={size}/>
+                    <div className='layout-wrapper'>
+                        <h3 className='layout-subtitle'>
+                            A Little <span className='underline text-blue-900'>About</span> Me.
+                        </h3>
+                        <p className='text-left mx-auto'> {text} </p>
+                    </div>
                 </div>
-                <div className='layout-wrapper'>
-                    <h3 className='layout-subtitle'>
-                        A Little <span className='underline text-blue-900'>About</span> Me.
-                    </h3>
-                    <p className='text-left mx-auto'> {text} </p>
-                </div>
-            </div>
+            </article>
         </section>
     );
 }
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -3,21 +3,22 @@ import React from 'react';
 import Image from 'next/image';
 
 function ExperienceCard() {
+    const size = 600
     return (
         <article
-            className='section-wrapper flex flex-col lg:flex-row items-center justify-center text-center px-4 md:px-10 lg:px-20 pt-16'>
+            className='flex flex-col lg:flex-row items-center justify-center text-center px-4 md:px-10 lg:px-20 pt-16'>
             <Link href='https://appnucleus.com.br/'
                   className='cursor-pointer my-2 mx-auto lg:w-1/2 xl:w-[600px] lg:rounded-full lg:rounded-md object-contain e'
                   title='Visit the App Nucleus'
             >
-                <Image className='layout-image bg-[#251857] max-h-72'
+                <Image className='layout-image px-8 bg-[#251857] max-h-72'
                        src="/logo_nucleus.png"
                        alt="Logo Nucleus Software last Company"
-                       width={600} height={600}
+                       width={size} height={size}
                 />
             </Link>
             <div className='layout-wrapper'>
-                <h3 className='layout-subtitle text-left'>Full-Stack Developer</h3>
+                <h3 className='layout-subtitle'>Full-Stack Developer</h3>
                 <div className='text-left'>
                     <p className='underline filter brightness-110 font-bold text-lg'>Nucleus Software</p>
                     <p>Sep 2021 · Aug 25 2023 · 2 years</p>
