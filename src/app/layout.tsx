01df1c5a2cf7b79de3a8b0ae2f4f26a8729b8d1c
Alex Cesar <thealexcesar@gmail.com>
2023-11-24 20:59:12 -0300

    configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,21 @@
-import type { Metadata as NextMetadata } from 'next';
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '././assets/stylesheets/style_tailwind.scss'
+import '././assets/stylesheets/style.sass';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
-interface CustomMetadata extends NextMetadata {
-    title: string;
-    description: string;
-    image: string;
-    author: string;
+export const metadata: Metadata = {
+  title: "Alex's Portfolio",
+  description: 'Web Developer - Computer Science Engineer',
 }
 
-export const metadata: CustomMetadata = {
-    title: "Alex's Portfolio",
-    description: 'Web Developer - Computer Science Engineer',
-    image: '/public/images/about.png',
-    author: 'Alex Cesar',
-};
-
-interface RootLayoutProps {
-    children: React.ReactNode;
-}
-
-function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en" data-theme="light">
-        <Head>
-            <title>{metadata.title}</title>
-            <meta name="description" content={metadata.description || ''} />
-            <meta property="og:image" content={metadata.image || ''} />
-            <meta property="og:author" content={metadata.author || ''} />
-        </Head>
-        <body className={inter.className}>{children}</body>
-        </html>
-    );
+function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en" data-theme="light">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
 }
 
 export default RootLayout;
