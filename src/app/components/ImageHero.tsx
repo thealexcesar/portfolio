eb933df537741ee0170c00a379d9db6a02ad335d
Alex Cesar <thealexcesar@gmail.com>
2023-12-06 17:22:28 -0300

    release 0.0.5

--- src/app/components/ImageHero.tsx
+++ src/app/components/ImageHero.tsx
@@ -6,8 +6,8 @@ function ImageHero() {
         <Image
             src='/default_avatar.png'
             alt="Alex's avatar"
-            height={800}
-            width={800}
+            height={400}
+            width={400}
             className='object-contain'
             priority={true}
         />
